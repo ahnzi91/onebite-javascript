2fa599be60dfa228677dd5ac7d9100e96169ad86
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-09-09 09:09:36 +0900

    한입 FE 챌린지 Day 01

---
+++ day01/main.js
@@ -0,0 +1,12 @@
+// Mission 1
+let num1 = 10;
+let num2 = 25;
+console.log(`두 변수의 합 : ${num1 + num2}`);
+
+  // Misson 2
+let typeOfNumber = 10;
+let typeOfString = "Hello World!";
+let typeOfUndefined;
+console.log(typeof(typeOfNumber));
+console.log(typeof(typeOfString));
+console.log(typeof(typeOfUndefined));
