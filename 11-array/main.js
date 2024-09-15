55be9b7bfde95097424e182a7353cfcf86595a13
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-09-15 21:56:47 +0900

    한입 FE 챌린지 Day 06

---
+++ 11-array/main.js
@@ -0,0 +1,12 @@
+let arr = new Array(1, 2, 3);
+let arr2 = new Array(3);
+
+console.log(arr);
+console.log(arr2);
+
+// 배열 리터럴
+let arr3 = [1, 2, 3];
+let arr4 = [3];
+
+console.log(arr3);
+console.log(arr4);
