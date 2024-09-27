3cf914ec683d281e6d7bf626369080d4357ecf49
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-09-27 10:14:03 +0900

    한 번에 끝내는 자바스크립트 - 섹션8. 상태 관리와 SPA - 2. 동물 앨범 만들기-2-2

--- section07-components-module-system/07-3-middle-project-moduling/src/App.js
+++ section07-components-module-system/07-3-middle-project-moduling/src/App.js
@@ -1,7 +1,48 @@
-import TabBar from "./components/TabBar";
-import Content from "./components/Content";
+import TabBar from "./components/TabBar.js";
+import Content from "./components/Content.js";
+import { request } from "./components/api.js";
 
 export default function App($app) {
-  const tabBar = new TabBar();
+  this.state = {
+    currentTab: "all",
+    photos: [],
+  };
+
+  const tabBar = new TabBar({
+    $app,
+    initialState: "",
+    // onClick()
+    // 1. 버튼이 클릭 될 때 실행되는 함수.
+    // 2. 버튼을 누르면 currentTab이라는 상태를 눌린 버튼의 값으로 변경 시켜야 한다.
+    // 3. 눌린 버튼의 상태에 맞는 API를 호출해 알맞은 데이터를 가져와야 한다.
+    // 4. 눌린 버튼에 대한 값은 name이라는 매개변수로 전달받는다.
+    onClick: async (name) => {
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos: await request(name),
+      });
+    },
+  });
   const content = new Content();
+
+  this.setState = (newState) => {
+    this.state = newState;
+    tabBar.setState(this.state.currentTab);
+    content.setState(this.state.photos);
+  };
+  // 웹 페이지에 초기 상태값을 설정해주는 init 함수 추가
+  const init = async () => {
+    try {
+      const initialPhotos = await request();
+      this.setState({
+        ...this.setState,
+        photos: initialPhotos,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  init();
 }
--- section07-components-module-system/07-3-middle-project-moduling/src/components/TabBar.js
+++ section07-components-module-system/07-3-middle-project-moduling/src/components/TabBar.js
@@ -1,1 +1,46 @@
-export default function TabBar() {}
+// TabBar 컴포넌트를 모듈로 사용할 예정 : export default 키워드 작성
+export default function TabBar({ $app, initialState, onClick }) {
+  this.status = initialState;
+  this.onClick = onClick;
+
+  this.$target = document.createElement("div");
+  this.$target.className = "tab-bar";
+  $app.appendChild(this.$target);
+
+  this.template = () => {
+    let temp = `
+      <div id="all">전체</div>
+      <div id="penguin">펭귄</div>
+      <div id="koala">코알라</div>
+      <div id="panda">판다</div>
+    `;
+
+    return temp;
+  };
+
+  // 화면 렌더해주는 함수
+  this.render = () => {
+    this.$target.innerHTML = this.template();
+
+    // 현재 state 값과 동일한 id를 갖는 버튼 요소를 가져와서 할당해준다.
+    // 해당 요소에 클래스 이름을 "clicked"로 설정해 배경 색상을 변경해준다.
+    let $currentTab = document.getElementById(this.state);
+    // $currentTab ? ($currentTab.className = "clicked") : "";
+    $currentTab && ($currentTab.className = "clicked");
+
+    const $tabBar = this.$target.querySelectorAll("div");
+    $tabBar.forEach((element) => {
+      element.addEventListener("click", () => {
+        onClick(element.id);
+      });
+    });
+  };
+
+  // state 값을 업데이트 하는 함수
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+
+  this.render();
+}
